c29de26e0fb1fa32c6055a2c772f1d0cd605ec1d
Gabriel Holanda de Freitas <gabriel.holanda@polealimentos.com.br>
2025-08-26 17:21:46 -0300

    Segundo Commit -  Mudando tipo da variável dataDeTermino para LocalDateTime criando classe 'FuncoesTarefa' com as principais funçoes da aplicação e 'Main' com o método Main

---
+++ src/aplicacao/todolist/logicadenegocios/FuncoesTarefa.java
@@ -0,0 +1,17 @@
+package aplicacao.todolist.logicadenegocios;
+
+public class FuncoesTarefa {
+	
+	public void criarTarefa() {
+		
+	}
+	
+	public void deletarTarefa() {
+		
+	}
+	
+	public void visualizarTarefa() {
+		
+	}
+	
+}
